c2726c278c4bf6ed95a6957d349982c46c18a6fa
carageaanda <anda.caragea99@gmail.com>
2022-09-20 18:04:36 +0300

    anda

---
+++ BulkyBook/BulkyBook.Utility/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+     
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("hello@anda.caragea.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("caragea.anda4@gmailcom", "Parolagmail1");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
